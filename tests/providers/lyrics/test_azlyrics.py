377f69364d1088e0c069caf8b1a2e317edc8accc
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2022-02-25 09:49:27 +0100

    fixing tests

--- tests/providers/lyrics/test_azlyrics.py
+++ tests/providers/lyrics/test_azlyrics.py
@@ -5,7 +5,6 @@ from spotdl.providers.lyrics.azlyrics import AzLyrics
 lyrics = "Always think about you\nAlways thought I have a life I could grow with you\nAs time fades, crazy how our lifes changed\nFeeling like a shockwave without you\n\nAlways think about you\nAlways thought I have a life I could grow with you\nAs time fades, crazy how our lifes changed\nFeeling like a shockwave without you\n\nAlways think about you\nAlways thought I have a life I could grow with you\nBut as time fades, crazy how our lifes changed\nFeeling like a shockwave without you\n\nFeeling like a shockwave, shockwave, shockwave, shockwave, shockwave, shockwave, shockwave, shockwave, shockwave\n\nFeeling like this shockwave\nGet out of this shockwave\n\nShockwave without you\nShockwave without you\n\nAlways think about you\nAlways thought I have a life I could grow with you\nAs time fades, crazy how our lifes changed\nFeeling like a shockwave without you\n\nAlways think about you\nAlways thought I have a life I could grow with you\nBut as time fades, crazy how our lifes changed\nFeeling like a shockwave without you\n\nAlways think about you\nAlways thought I have a life I could grow with you\nBut as time fades, crazy how our lifes changed\nFeeling like a shockwave without you\n\nFeeling like a shockwave, shockwave, shockwave, shockwave, shockwave, shockwave, shockwave, shockwave, shockwave\n\nFeeling like this shockwave\nGet out of this shockwave\n"
 
 
-@pytest.mark.vcr()
 def test_get_azlyrics_lyrics():
     azlyrics = AzLyrics()
 
--- tests/providers/lyrics/test_musixmatch.py
+++ tests/providers/lyrics/test_musixmatch.py
@@ -5,7 +5,6 @@ from spotdl.providers.lyrics.musixmatch import MusixMatch
 lyrics = "Stranded in the open\nDried out tears of sorrow\nLacking all emotion\nStaring down the barrel waiting for the\nFinal gates to open\nTo a new tomorrow\nMoving with the motion\nFollowing the light that sets me free\n\nSets me free"
 
 
-@pytest.mark.vcr()
 def test_get_musixmatch_lyrics():
     musixmatch = MusixMatch()
 
